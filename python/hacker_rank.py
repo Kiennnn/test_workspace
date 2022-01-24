e91376a089cd7f1bf354eed5b6df3b9d32402149
kienn <kienn@email.com>
2022-01-24 23:41:27 +0700

    String Formatting

--- python/hacker_rank.py
+++ python/hacker_rank.py
@@ -1,18 +1,16 @@
 #! /usr/bin/env python3
 
-N, M = map(int,input().split())
-x='.|.'
-top = int(N/2)  # upper half
-mid = top + 1  # middle line
+def print_formatted(number):
+    # your code goes here
+    len = int(number).bit_length()
+    for i in range (1, number+1, 1):
+        d = str(i).upper()
+        o = str(oct(i)).upper()
+        h = str(hex(i)).upper()
+        b = str(bin(i)).upper()
+        ls = [d.rjust(len), o[2:].rjust(len), h[2:].rjust(len), b[2:].rjust(len)]
+        print(*ls)
 
-for i in range (1,N+1,1):
-    if i <= top:
-        print((x*(1 + (i-1)*2)).center(M,'-'))
-    elif i == mid:
-        print('WELCOME'.center(M,'-'))
-    else:
-        print((x*(1 + (top-1)*2 - 2*(i-mid-1))).center(M,'-'))
-
-# 1 line
-# for i in range(N):
-#     print((x*(1+i*2)).center(M,'-') if i in range(int(N/2)) else 'WELCOME'.center(M,'-') if i==int(N/2) else (x*((int(N/2)-1)*2+1-(i-int(N/2)-1)*2)).center(M,'-'))
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
