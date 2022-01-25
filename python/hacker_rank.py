7492bd9a557d062d34e33d13b7841e7f64650dd2
kienn <kienn@email.com>
2022-01-26 00:20:02 +0700

    Capitalize!

--- python/hacker_rank.py
+++ python/hacker_rank.py
@@ -1,18 +1,27 @@
 #! /usr/bin/env python3
 
-N, M = map(int,input().split())
-x='.|.'
-top = int(N/2)  # upper half
-mid = top + 1  # middle line
+import os
 
-for i in range (1,N+1,1):
-    if i <= top:
-        print((x*(1 + (i-1)*2)).center(M,'-'))
-    elif i == mid:
-        print('WELCOME'.center(M,'-'))
-    else:
-        print((x*(1 + (top-1)*2 - 2*(i-mid-1))).center(M,'-'))
+# Complete the solve function below.
+def solve(s):
+    s = list(s)
+    if s[0].isalpha():
+        s[0] = s[0].upper()
+    for i in range(len(s)):
+        if s[i] == ' ':
+            s[i+1] = s[i+1].upper()
+    s = ''.join(s)
+    return s
 
-# 1 line
-# for i in range(N):
-#     print((x*(1+i*2)).center(M,'-') if i in range(int(N/2)) else 'WELCOME'.center(M,'-') if i==int(N/2) else (x*((int(N/2)-1)*2+1-(i-int(N/2)-1)*2)).center(M,'-'))
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    print(result)
+
+    # fptr.write(result + '\n')
+
+    # fptr.close()
