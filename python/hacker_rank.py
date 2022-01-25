2823ca6906fe17983fffd28235d5ed91449a9366
kienn <kienn@email.com>
2022-01-25 23:56:12 +0700

    Alphabet Rangoli

--- python/hacker_rank.py
+++ python/hacker_rank.py
@@ -1,16 +1,23 @@
 #! /usr/bin/env python3
+import string
 
-def print_formatted(number):
+def reverse_list(my_list):
+    my_list.reverse()
+    return my_list
+
+def print_rangoli(size):
     # your code goes here
-    len = int(number).bit_length()
-    for i in range (1, number+1, 1):
-        d = str(i).upper()
-        o = str(oct(i)).upper()
-        h = str(hex(i)).upper()
-        b = str(bin(i)).upper()
-        ls = [d.rjust(len), o[2:].rjust(len), h[2:].rjust(len), b[2:].rjust(len)]
-        print(*ls)
+    alphabet = list(string.ascii_lowercase)
+    line_number = size*2 - 1
+    for i in range (line_number):
+        if i < size:
+            line = ''.join(reverse_list(alphabet[(size-i):(size)])) + alphabet[size-1-i] + ''.join(alphabet[(size-i):(size)])
+        else:
+            line = ''.join(reverse_list(alphabet[(i-size+2):(size)])) + alphabet[i-size+1] + ''.join(alphabet[(i-size+2):(size)])
+
+        joined_line = '-'.join(line)
+        print(joined_line.center(4*size-3,'-'))  # 2n-1 letters plus 2n-2 '-' characters
 
 if __name__ == '__main__':
     n = int(input())
-    print_formatted(n)
+    print_rangoli(n)
