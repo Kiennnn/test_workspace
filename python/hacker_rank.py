189179b13b0d12b5e5fda9d7c4d44796503ec398
kienn <kienn@email.com>
2022-01-27 23:01:22 +0700

    collections.Counter()

--- python/hacker_rank.py
+++ python/hacker_rank.py
@@ -1,5 +1,17 @@
 #! /usr/bin/env python3
 
-a = ()
-c = ()
-b = ()
+from collections import Counter
+
+shoes = int(input())
+shoe_sizes = list(map(int, input().split()))
+size_counter = Counter(shoe_sizes)
+customers= int(input())
+total_price = 0
+for i in range(customers):
+    shoe_size, price = map(int, input().split())
+    if shoe_size in shoe_sizes and size_counter[shoe_size] != 0:
+        total_price += price
+        size_counter[shoe_size] -= 1
+    else:
+        continue
+print(total_price)
