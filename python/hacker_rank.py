e42fa247c90af47334c18144d558718d188ce5c7
kienn <kienn@email.com>
2022-01-27 20:24:22 +0700

    tuple a

--- python/hacker_rank.py
+++ python/hacker_rank.py
@@ -1,27 +1,3 @@
 #! /usr/bin/env python3
 
-import os
-
-# Complete the solve function below.
-def solve(s):
-    s = list(s)
-    if s[0].isalpha():
-        s[0] = s[0].upper()
-    for i in range(len(s)):
-        if s[i] == ' ':
-            s[i+1] = s[i+1].upper()
-    s = ''.join(s)
-    return s
-
-if __name__ == '__main__':
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    s = input()
-
-    result = solve(s)
-
-    print(result)
-
-    # fptr.write(result + '\n')
-
-    # fptr.close()
+a = ()
