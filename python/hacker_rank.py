765d64630160ae323fc259abb64c56481a1cc83c
kienn <kienn@email.com>
2022-01-28 20:39:47 +0700

    set.intersection()

--- python/hacker_rank.py
+++ python/hacker_rank.py
@@ -1,17 +1,8 @@
 #! /usr/bin/env python3
 
-from collections import Counter
-
-shoes = int(input())
-shoe_sizes = list(map(int, input().split()))
-size_counter = Counter(shoe_sizes)
-customers= int(input())
-total_price = 0
-for i in range(customers):
-    shoe_size, price = map(int, input().split())
-    if shoe_size in shoe_sizes and size_counter[shoe_size] != 0:
-        total_price += price
-        size_counter[shoe_size] -= 1
-    else:
-        continue
-print(total_price)
+n = int(input())
+eng_set = set(input().split())
+b = int(input())
+fre_set = set(input().split())
+inter_set = eng_set.intersection(fre_set)
+print(len(inter_set))
