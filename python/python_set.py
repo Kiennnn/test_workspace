d246f7e4e650924910c38c12797cff43c22f6e58
kienn <kienn@email.com>
2022-01-31 00:18:01 +0700

    Check Strict Superset

--- python/python_set.py
+++ python/python_set.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python3
-
-from collections import Counter as cnt
-
-K = int(input())
-room_list = input().split()
-room_set = set(room_list)  # to eliminate duplicate elements
-room_without_captain = set((cnt(room_list)-cnt(room_set)).elements())  # remain family's rooms
-captain_room = list(room_set.difference(room_without_captain))
-print(captain_room[0])
+A = set(input().split())
+n = int(input())
+answer = True
+for i in range(n):
+    N = set(input().split())
+    diff = A.difference(N)
+    if len(diff) > 0 and A.intersection(N) == N:
+        continue
+    else:
+        answer = False
+        break
+print(answer)
